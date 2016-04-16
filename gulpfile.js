6de89b0a96355fd8978b036201d7ce1f90905942
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-15 18:43:29 -0700

    Add CSS concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp'),
-    less = require('gulp-less');
+  less = require('gulp-less'),
+  clean = require('gulp-clean'),
+  concatCSS = require('gulp-concat-css');
 
-gulp.task('convertLess', function() {
+gulp.task('convertLess', function () {
   return gulp.src('src/**/*.less')
     .pipe(less())
     .pipe(gulp.dest('src'));
 });
+
+gulp.task('clean', function () {
+  return gulp.src('dist')
+    .pipe(clean());
+});
+
+gulp.task('concatCSS', function() {
+  return gulp.src('src/**/*.css')
+    .pipe(concatCSS('teenytiny.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['clean', 'concatCSS']);
