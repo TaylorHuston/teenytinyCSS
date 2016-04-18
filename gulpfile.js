4c34b445840ecb8615385e9634c9e25a111f2800
Taylor Huston <taylor.a.huston@gmail.com>
2016-04-18 00:07:38 -0700

    Add AutoPrefixee

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 var gulp = require('gulp'),
   less = require('gulp-less'),
   clean = require('gulp-clean'),
-  concatCSS = require('gulp-concat-css');
-
-gulp.task('convertLess', function () {
-  return gulp.src('src/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('src'));
-});
+  concatCSS = require('gulp-concat-css'),
+  prefix = require('gulp-autoprefixer');
 
 gulp.task('clean', function () {
   return gulp.src('dist')
     .pipe(clean());
 });
 
-gulp.task('concatCSS', function() {
+gulp.task('convertLess', ['clean'], function () {
+  return gulp.src('src/**/*.less')
+    .pipe(less())
+    .pipe(prefix({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('src'));
+});
+
+gulp.task('concatCSS', ['convertLess'], function () {
   return gulp.src('src/**/*.css')
     .pipe(concatCSS('teenytiny.css'))
     .pipe(gulp.dest('dist/css'));
